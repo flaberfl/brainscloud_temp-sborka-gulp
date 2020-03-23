acf62924b99c2f724cc13110a6ae8e05dd44f292
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-23 23:13:10 +0300

    main template

--- gulpfile.js
+++ gulpfile.js
@@ -57,8 +57,16 @@ function browserSyncReload(done) {
 }
 
 function html() {
+  panini.refresh();
   return src(path.src.html, { base: "src/" })
     .pipe(plumber())
+    .pipe(panini({
+      root: 'src/',
+      layouts: 'src/tpl/layouts/',
+      partials: 'src/tpl/partials/',
+      helpers: 'src/tpl/helpers/',
+      data: 'src/tpl/data/'
+    }))
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream());
 }
@@ -68,7 +76,7 @@ function css() {
     .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ['last 8 versions'],
+      Browserslist: ['last 8 versions'],
       cascade: true
     }))
     .pipe(cssbeautify())
