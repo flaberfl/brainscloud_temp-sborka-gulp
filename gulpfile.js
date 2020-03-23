a0a42354726e4dadb3f0ef7c261479ea0c241ea4
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-23 18:16:22 +0300

    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const {
-  src,
-  dest
-} = require("gulp");
+const {src, dest} = require("gulp");
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
@@ -20,111 +17,106 @@ const panini = require("panini");
 const browsersync = require("browser-sync").create();
 
 
+/* Paths */
 var path = {
-  build: {
-    html: "dist/",
-    js: "dist/assets/js/",
-    css: "dist/assets/css/",
-    images: "dist/assets/img/"
-  },
-  src: {
-    html: "src/*.html",
-    js: "src/assets/js/*.js",
-    css: "src/assets/sass/style.scss",
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
-  },
-  watch: {
-    html: "src/**/*.html",
-    js: "src/assets/js/**/*.js",
-    css: "src/assets/sass/**/*.scss",
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
-  },
-  clean: "./dist"
+    build: {
+        html: "dist/",
+        js: "dist/assets/js/",
+        css: "dist/assets/css/",
+        images: "dist/assets/img/"
+    },
+    src: {
+        html: "src/*.html",
+        js: "src/assets/js/*.js",
+        css: "src/assets/sass/style.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+    },
+    watch: {
+        html: "src/**/*.html",
+        js: "src/assets/js/**/*.js",
+        css: "src/assets/sass/**/*.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+    },
+    clean: "./dist"
 }
+
 
 
 /* Tasks */
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./dist/"
-    },
-    port: 3000
-  });
+    browsersync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 3000
+    });
 }
 
 function browserSyncReload(done) {
-  browsersync.reload();
+    browsersync.reload();
 }
 
 function html() {
-  return src(path.src.html, { base: "src/" })
-    .pipe(plumber())
-    .pipe(panini({
-      root: 'pages/',
-      layouts: 'layouts/',
-      partials: 'partials/',
-      helpers: 'helpers/',
-      data: 'data/'
-    }))
-    .pipe(dest(path.build.html))
-    .pipe(browsersync.stream());
+    return src(path.src.html, { base: "src/" })
+        .pipe(plumber())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
 }
 
 function css() {
-  return src(path.src.css, { base: "src/assets/sass/" })
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 8 versions'],
-      cascade: true
-    }))
-    .pipe(cssbeautify())
-    .pipe(dest(path.build.css))
-    .pipe(cssnano({
-      zindex: false,
-      discardComments: {
-        removeAll: true
-      }
-    }))
-    .pipe(removeComments())
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".css"
-    }))
-    .pipe(dest(path.build.css))
-    .pipe(browsersync.stream());
+    return src(path.src.css, { base: "src/assets/sass/" })
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 8 versions'],
+            cascade: true
+        }))
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(removeComments())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
 }
 
 function js() {
-  return src(path.src.js, { base: './src/assets/js/' })
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".js"
-    }))
-    .pipe(dest(path.build.js))
-    .pipe(browsersync.stream());
+    return src(path.src.js, {base: './src/assets/js/'})
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
 }
 
 function images() {
-  return src(path.src.images)
-    .pipe(imagemin())
-    .pipe(dest(path.build.images));
+    return src(path.src.images)
+        .pipe(imagemin())
+        .pipe(dest(path.build.images));
 }
 
 function clean() {
-  return del(path.clean);
+    return del(path.clean);
 }
 
 function watchFiles() {
-  gulp.watch([path.watch.html], html);
-  gulp.watch([path.watch.css], css);
-  gulp.watch([path.watch.js], js);
-  gulp.watch([path.watch.images], images);
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.images], images);
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, js, images));
